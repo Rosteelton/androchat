b06adf29d499096b70ed1c91e7f564baae83d188
Rosteelton <Rosteelton@gmail.com>
2017-04-18 23:43:18 +0300

    Prepare splash screen animation

---
+++ app/src/main/java/com/steelcrow/androchat/SplashScreenActivity.java
@@ -0,0 +1,47 @@
+package com.steelcrow.androchat;
+
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private ImageView octopus;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        octopus = (ImageView) findViewById(R.id.splashscreen);
+        Log.d("qwertyyyyyy","123");
+
+        AnimatedVectorDrawableCompat drawableCompat = AnimatedVectorDrawableCompat.create(this, R.drawable.ic_octopus);
+        octopus.setImageDrawable(drawableCompat);
+        drawableCompat.start();
+
+//        AnimatedVectorDrawableCompat animatedVector = AnimatedVectorDrawableCompat.create(this, R.drawable.ic_octopus);
+//        octopus.setImageDrawable(animatedVector);
+//        animatedVector.start();
+
+//        Drawable drawable = octopus.getDrawable();
+//        if (drawable instanceof Animatable){
+//            Log.d("qwertyyyyyy","werqwerqwerqwerqwerqwerjhjfghjfghjfghjfgjhfghjfghjfgjh");
+//            ((Animatable) drawable).start();
+//        }
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
