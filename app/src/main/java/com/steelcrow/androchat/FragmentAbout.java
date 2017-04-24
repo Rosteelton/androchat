8ff776d8b3bf4e7beafa8c66f1507e32b32d01ca
Rosteelton <Rosteelton@gmail.com>
2017-04-24 21:24:20 +0300

    Add animation in about fragment

--- app/src/main/java/com/steelcrow/androchat/FragmentAbout.java
+++ app/src/main/java/com/steelcrow/androchat/FragmentAbout.java
@@ -4,9 +4,11 @@ import android.support.annotation.Nullable;
 import android.support.transition.TransitionManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.steelcrow.androchat.navigation.NavigationActivity;
@@ -27,26 +29,29 @@ public class FragmentAbout extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         title = getArguments().getString("title");
-
-
-
-
     }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
 
         //animation
-//        ImageView personView = (ImageView) container.findViewById(R.id.imageView_about);
-//        int newWidth = getResources().getDimensionPixelSize(R.dimen.person_about_max_width);
-//        int newHeight = getResources().getDimensionPixelSize(R.dimen.person_about_max_height);
-//        TransitionManager.beginDelayedTransition(container);
-//        ViewGroup.LayoutParams params = personView.getLayoutParams();
-//        params.width = newWidth;
-//        params.height = newHeight;
-//        personView.setLayoutParams(params);
+        final ImageButton personView = (ImageButton) view.findViewById(R.id.imageView_about);
+        personView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newWidth = getResources().getDimensionPixelSize(R.dimen.person_about_max_width);
+                int newHeight = getResources().getDimensionPixelSize(R.dimen.person_about_max_height);
+                TransitionManager.beginDelayedTransition(container);
+                ViewGroup.LayoutParams params = personView.getLayoutParams();
+                params.width = newWidth;
+                params.height = newHeight;
+                personView.setLayoutParams(params);
+            }
+        });
+
+
 
         NavigationActivity activity = (NavigationActivity) getActivity();
         activity.getSupportActionBar().setTitle(title);
