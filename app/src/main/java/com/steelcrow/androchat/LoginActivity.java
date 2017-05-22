6a41c6439109426c550b1189403c16133a1919c1
Rosteelton <Rosteelton@gmail.com>
2017-05-22 22:38:00 +0300

    WIP

--- app/src/main/java/com/steelcrow/androchat/LoginActivity.java
+++ app/src/main/java/com/steelcrow/androchat/LoginActivity.java
@@ -16,17 +16,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.steelcrow.androchat.navigation.NavigationActivity;
 import com.steelcrow.androchat.service.LoginService;
 import com.steelcrow.androchat.widgets.ProgressButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-
     public static final String LOGIN = "login";
     public static final String PASSWORD = "password";
     public static final String PENDING_INTENT = "pi";
+    public static final String TAG = "LoginActivity";
 
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     public static final int LOGIN_REQUEST_CODE = 1;
 
@@ -35,9 +43,44 @@ public class LoginActivity extends AppCompatActivity {
     private ProgressButton button;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acivity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Intent intent = new Intent(LoginActivity.this, NavigationActivity.class);
+                    intent.putExtra("Login", user.getDisplayName());
+                    startActivity(intent);
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
         login = (TextView) findViewById(R.id.edit_text_login);
         password = (TextView) findViewById(R.id.edit_text_password);
         button = (ProgressButton) findViewById(R.id.btn_enter);
@@ -70,6 +113,23 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public void createNewAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            new LoginActivity.MyDialogFragment().show(getSupportFragmentManager(), "AlertDialog");
+                        }
+                    }
+                });
     }
 
     public static class MyDialogFragment extends DialogFragment {
